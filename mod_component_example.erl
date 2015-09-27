6b29eff6622ee6b1d609363c1ac877fe0e249fd2
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2015-09-27 11:05:10 +0200

    Setting up

---
+++ mod_component_example.erl
@@ -0,0 +1,8 @@
+-module(mod_component_example).
+-author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
+
+-mod_title("Mod_component examples").
+-mod_description("A module with some examples on using mod_component.").
+-mod_prio(20).
+
+-mod_depends([component]).
